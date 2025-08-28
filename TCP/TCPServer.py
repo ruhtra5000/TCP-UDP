cc99253478fa099d525f6336fc9cb96bf21cea18
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-08-28 16:44:07 -0300

    feat - adding measurement functions to TCPServer

--- TCP/TCPServer.py
+++ TCP/TCPServer.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 # Definição de IP e porta
 HOST = '127.0.0.1'
@@ -14,12 +15,93 @@ print("Servidor TCP aguardando conexões...")
 conn, addr = server.accept()
 print(f"Conectado por {addr}")
 
-# Recebimento de mensagens TCP
-while True:
-    data = conn.recv(1024).decode()
-    if not data:
-        break
-    print(f"Recebido: {data}")
-    conn.sendall("Mensagem recebida".encode())
+# Dicionário de informações relevantes
+info = {"amount_pck": 0, "lost_pck": 0}
 
-conn.close()
+# Vetor de dados recebidos
+dataRecieved = []
+
+# Calculo de RTT
+def calculateRTT(timeSend):
+    return time.time_ns() - int(timeSend)
+
+# Calculo de Jitter 
+def calculateJitter(rtt):
+    if not dataRecieved:
+        return 0
+
+    jitter = rtt - dataRecieved[-1]["RTT"]
+    
+    return jitter if jitter > 0 else 0
+
+# Checagem da ordenação dos pacotes
+def verifyPackageOrder(actualSeq):
+    if dataRecieved:
+        if int(actualSeq) != dataRecieved[-1]["seq"] + 1:
+            return False # Ordem errada
+        
+        return True # Ordem correta
+    else:
+        # Deve ser o pacote com seq 0 (Primeiro pacote)
+        if int(actualSeq) != 0:
+            return False
+        
+        return True
+    
+# Calculo de Throughput
+def calculateThroughput(totalTime):
+    sizeRecieved = 0
+    
+    for data in dataRecieved:
+        sizeRecieved += data["len"]
+
+    return (sizeRecieved*8) / totalTime
+
+# "Main"
+# Recebimento de mensagens TCP
+def TCPServer():
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+
+        try:
+            seq, timeSend, len, payload = data.split('|', 3) # Divisão da mensagem
+            
+            rtt = calculateRTT(timeSend)
+            jitter = calculateJitter(rtt)
+            
+            if not verifyPackageOrder(seq):
+                info["lost_pck"] += 1
+
+            # Salvando dados do pacote recebido
+            dataRecieved.append({
+                "seq": int(seq), 
+                "timeSend": int(timeSend), 
+                "len": int(len), 
+                "payload": payload, 
+                "RTT": rtt, 
+                "jitter": jitter
+            })
+            info["amount_pck"] += 1
+
+        except ValueError:
+            print("Mensagem mal formatada:", data)
+
+        # Resposta do servidor
+        conn.sendall("Mensagem recebida".encode())
+
+    conn.close()
+
+# Inicialização do servidor (marcando tempo total de operação)
+start = time.time()
+TCPServer()
+end = time.time()
+
+# Impressão de dados pós-operação
+print(f'\n\ninfo: {info}\n')
+
+for data in dataRecieved:
+    print(f"{data}\n")
+
+print(f"Throughput (bytes/s): {calculateThroughput(end - start)}\n")
