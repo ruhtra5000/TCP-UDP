66aaf6ec6862718fc32846516ad064a2eef3a108
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-08-27 23:33:49 -0300

    style - adding code comments

--- TCP/TCPClient.py
+++ TCP/TCPClient.py
@@ -1,11 +1,14 @@
 import socket
 
+# Definição de IP e porta
 HOST = '127.0.0.1'
 PORT = 5000
 
+# Criação do socket do cliente 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
+# Envio de mensagens TCP
 for i in range(5):
     msg = f"Mensagem TCP {i+1}"
     client.sendall(msg.encode())
--- TCP/TCPServer.py
+++ TCP/TCPServer.py
@@ -1,16 +1,20 @@
 import socket
 
+# Definição de IP e porta
 HOST = '127.0.0.1'
 PORT = 5000
 
+# Abertura do socket do servidor
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
+# Handshake TCP
 print("Servidor TCP aguardando conexões...")
 conn, addr = server.accept()
 print(f"Conectado por {addr}")
 
+# Recebimento de mensagens TCP
 while True:
     data = conn.recv(1024).decode()
     if not data:
