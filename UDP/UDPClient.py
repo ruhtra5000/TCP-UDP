52dc28946705e37bb125dd2de0ea2dd0053ada07
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-08-28 16:22:05 -0300

    feat - adding new info at UDP message

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 # Definição de IP e porta
 HOST = '127.0.0.1'
@@ -8,9 +9,15 @@ PORT = 5001
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Realiza o envio de mensagens UDP
-for i in range(5):
-    msg = f"Mensagem UDP {i+1}"
-    client.sendto(msg.encode(), (HOST, PORT))
+for seq in range(5):
+    timeSend = time.time_ns()
+    payload = f"Mensagem UDP {seq+1}"
+
+    # Mesangem com: número de sequencia, horário do envio, 
+    # tamanho do conteúdo, e o conteúdo em si
+    completeMessage = f"{seq}|{timeSend}|{len(payload)}|{payload}"
+    
+    client.sendto(completeMessage.encode(), (HOST, PORT))
     data, _ = client.recvfrom(1024)
     print(f"Servidor respondeu: {data.decode()}")
 
