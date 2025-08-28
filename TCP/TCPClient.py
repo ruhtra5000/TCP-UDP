f4fb9d276f01596eb8b6087a11a309500efd4fb5
Arthur de Sá Tenório <arthursa147@gmail.com>
2025-08-28 16:21:38 -0300

    feat - adding new info at TCP message

--- TCP/TCPClient.py
+++ TCP/TCPClient.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 # Definição de IP e porta
 HOST = '127.0.0.1'
@@ -9,9 +10,15 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
 # Envio de mensagens TCP
-for i in range(5):
-    msg = f"Mensagem TCP {i+1}"
-    client.sendall(msg.encode())
+for seq in range(5):
+    timeSend = time.time_ns()
+    payload = f"Mensagem TCP {seq+1}"
+
+    # Mesangem com: número de sequencia, horário do envio, 
+    # tamanho do conteúdo, e o conteúdo em si
+    completeMessage = f"{seq}|{timeSend}|{len(payload)}|{payload}"
+    
+    client.sendall(completeMessage.encode())
     resposta = client.recv(1024).decode()
     print(f"Servidor respondeu: {resposta}")
 
